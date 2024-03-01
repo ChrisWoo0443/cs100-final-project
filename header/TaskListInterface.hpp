d546c57df7ebcdf240255280b818b278f76a0b60
nitemUCR <nitem003@ucr.edu>
2024-03-01 14:49:08 -0800

    Added implementation for interface abstract class and tasklistinterface

---
+++ header/TaskListInterface.hpp
@@ -0,0 +1,20 @@
+#ifndef TASKLIST_INTERFACE_HPP
+#define TASKLIST_INTERFACE_HPP
+
+#include "Interface.hpp"
+#include "TaskList.hpp"
+#include <vector>
+#include <iostream>
+
+class TaskListInterface: public Interface {
+    protected:
+    std::vector<TaskList> taskLists;
+    void PrintBody() const override;
+    void PrintFooter() const override;
+    
+    public:
+    TaskListInterface(const std::string& input, const std::vector<TaskList>& taskLists);
+    
+};
+
+#endif
