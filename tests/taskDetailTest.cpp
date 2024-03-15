24fd4217b14eda71baac62830efb32342f6a22de
ChrisWoo0443 <chefwoodini@gmail.com>
2024-03-14 21:55:12 -0700

    working on inputValidation tests and taskview tests

--- tests/taskDetailTest.cpp
+++ tests/taskDetailTest.cpp
@@ -5,13 +5,21 @@
 #include "../header/taskView.hpp"
 #include "../header/Task.hpp"
 #include "../header/TaskList.hpp"
+#include "../header/Interface.hpp"
 using namespace std;
 
 TEST(displayTest, displayTaskDetailsTest){
     Task testTask("hw");
     testTask.editDetail("this is math hw");
     testTask.editPriority(1);
-    TaskView printTask;
-    string expectedString = "TASK IN DETAIL:\nTask Name: hw\nDescription: this is math hw\nPriority: 1";
+    TaskView printTask("hw", testTask);
+    string expectedString = "============================================================"
+                            "| hw"
+                            "============================================================"
+                            "| Description: this is math hw"
+                            "| Priority: 1"
+                            "| Due: 3/15/24"
+                            "============================================================"
+                            "| To see a list of commands, type ?help.\n";
     EXPECT_EQ(printTask.displayTaskDetails(testTask), expectedString);
 }
